861a91783e4d632e97a6ae20995c5ce2bca693e6
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2023-03-07 15:36:00 +0900

    feat: [1-1] 프로그래머스 괄호 회전하기

--- 1-1-gm.js
+++ 1-1-gm.js
@@ -1,47 +1,48 @@
+// 괄호 회전하기
 function solution(s) {
-  // 문자열의 길이
-  const strLength = s.length;
+  const isRightBracket = (arr) => {
+    // 원본 배열을 받아 복사
+    const copyArr = arr.slice();
+    const stack = [];
 
-  // 자를 단위
-  let unit = 1;
-
-  // 문자열의 절반까지만 검사하면 된다.
-  const unitMax = strLength / 2;
-
-  // 가장 짧은 길이를 구하는 변수 - 초기화.
-  let minLength = strLength;
-
-  for (let i = 1; i <= unitMax; i++) {
-    // 앞에서부터 자르는 문자열
-    let unitString = s.slice(0, i);
-
-    // 카운트
-    let cnt = 1;
-
-    // 누적해서 문자열 저장
-    let tempString = "";
-
-    for (let j = i; j < strLength; j += i) {
-      // 비교할 문자열
-      const comparedString = s.slice(j, j + i);
-
-      // 같다면 카운트를 증가시키고 넘어가고
-      // 다를 경우 문자열에 누적해준 후 unitString을 초기화한다.
-      if (unitString === comparedString) {
-        cnt++;
+    while (copyArr.length) {
+      if (!stack.length) {
+        // 스택이 비어있다면 push
+        stack.push(copyArr.shift());
       } else {
-        if (cnt > 1) tempString += cnt.toString();
-        tempString += unitString;
-        unitString = comparedString;
-        cnt = 1;
+        // arr의 첫 번쨰 원소
+        const bracket = copyArr[0];
+        // stack의 마지막 원소
+        const lastOfStack = stack[stack.length - 1];
+
+        if (
+          // 쌍을 이루는 경우
+          (bracket === ")" && lastOfStack === "(") ||
+          (bracket === "]" && lastOfStack === "[") ||
+          (bracket === "}" && lastOfStack === "{")
+        ) {
+          stack.pop();
+          copyArr.shift();
+        } else {
+          // 쌍을 이루지 않는 경우
+          stack.push(copyArr.shift());
+        }
       }
     }
-    // 마지막에 수행된 부분을 마찬가지로 문자열에 누적.
-    if (cnt > 1) tempString += cnt.toString();
-    tempString += unitString;
+    if (stack.length === 0) return true;
+  };
 
-    // 최소값을 갱신.
-    minLength = Math.min(minLength, tempString.length);
+  // 문자열을 회전하면서 올바른 문자열일 경우 cnt 증가
+  let cnt = 0;
+  let rotate = 0;
+  // 문자열을 배열로 변환
+  const bracketArr = s.split("");
+
+  while (rotate < bracketArr.length) {
+    if (isRightBracket(bracketArr)) cnt++;
+    bracketArr.push(bracketArr.shift());
+    rotate++;
   }
-  return minLength;
+
+  return cnt;
 }
